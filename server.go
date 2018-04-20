d67dd468d9184094e32ce76b950d69c9bdd2ec5d
mexskican <benoit.verkindt@gmail.com>
2018-04-20 14:47:05 +0200

    Set contracts in session variable
    To read the contract it is always done with the same parameters,
    it doesn't need to redeclare the contract for each call.
    
    To write information on the contract the user should add the
    content of his key json.

--- server.go
+++ server.go
@@ -17,8 +17,13 @@ import (
 	"./token"
 )
 
+var (
+	bikeSession *bike.BikeSession
+	tokenSession *token.BikeTokenSession
+)
+
 const (
-	key = "paste the contents of your *rinkeby* key json here"
+	key = ``
 	infuraEndpoint = "https://rinkeby.infura.io/SlVwORwAFmkf4EHiccur"
 	localEndpoint = "/home/mexskican/.ethereum/testnet/geth.ipc"
 	bikeContractAddress = "0x903b552d9dba24bbb908a064d8568b76923d5501"
@@ -27,31 +32,22 @@ const (
 
 
 func bikeHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := ethclient.Dial(infuraEndpoint)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum network: %v", err)
-	}
-	contract, err := bike.NewBike(common.HexToAddress(bikeContractAddress), conn)
-	if err != nil {
-		log.Fatalf("Failed to instantiate contract: %v", err)
-	}
-
 	// TODO: Add regular expression to validate the params
 	method := r.URL.Path[len("/view-bike/"):]
 	if strings.Compare("hourlycost", method) == 0 {
-		hourlyCost, _ := contract.HourlyCost(&bind.CallOpts{})
+		hourlyCost, _ := bikeSession.HourlyCost()
 		fmt.Fprintf(w, "<h1>Hourly Cost:</h1><div>%d</div>", hourlyCost)
 	} else if strings.HasPrefix(method, "isavailable") {
 		id, _ := strconv.ParseInt(r.URL.Path[len("/view/isavailable/"):], 10, 64)
-		isAvailable, _ := contract.BikeIsAvailable(&bind.CallOpts{}, big.NewInt(id))
+		isAvailable, _ := bikeSession.BikeIsAvailable(big.NewInt(id))
 		fmt.Fprintf(w, "<h1>Is available:</h1><div>%t</div>", isAvailable)
 	} else if strings.HasPrefix(method, "bikeid") {
 		address := r.URL.Path[len("/view-bike/bikeid/"):]
-		bikeId, _ := contract.GetRentalBikeId(&bind.CallOpts{}, common.HexToAddress(address))
+		bikeId, _ := bikeSession.GetRentalBikeId(common.HexToAddress(address))
 		fmt.Fprintf(w, "<h1>Bike Id of %s:</h1><div>%d</div>", address, bikeId)
 	} else if strings.HasPrefix(method, "returntime") {
 		address := r.URL.Path[len("/view-bike/returntime/"):]
-		returnTime, _ := contract.GetReturnTime(&bind.CallOpts{}, common.HexToAddress(address))
+		returnTime, _ := bikeSession.GetReturnTime(common.HexToAddress(address))
 		fmt.Fprintf(w, "<h1>Return Time of %s:</h1><div>%d</div>", address, returnTime)
 	} else {
 		fmt.Fprintf(w, "<h1>Wrong Parameter<h1>")
@@ -59,26 +55,17 @@ func bikeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func tokenHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := ethclient.Dial(infuraEndpoint)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum network: %v", err)
-	}
-	contract, err := token.NewBikeToken(common.HexToAddress(tokenContractAddress), conn)
-	if err != nil {
-		log.Fatalf("Failed to instantiate contract: %v", err)
-	}
-
 	method := r.URL.Path[len("/view-token/"):]
 	if strings.Compare("name", method) == 0 {
-		name, _ := contract.Name(&bind.CallOpts{})
+		name, _ := tokenSession.Name()
 		fmt.Fprintf(w, "<h1>Token Name:</h1><div>%s</div>", name)
 	} else if strings.Compare("price", method) == 0 {
-		price, _ := contract.PRICE(&bind.CallOpts{})
+		price, _ := tokenSession.PRICE()
 		fmt.Fprintf(w, "<h1>Token price:</h1><div>%d</div>", price)
 	} else if strings.HasPrefix(method, "balance") {
 		address := r.URL.Path[len("/view-token/balance/"):]
 		balance := big.NewInt(0)
-		balance, _ = contract.BalanceOf(&bind.CallOpts{}, common.HexToAddress(address))
+		balance, _ = tokenSession.BalanceOf(common.HexToAddress(address))
 		fmt.Fprintf(w, "<h1>Balance of %s:</h1><div>%d</div>",address, balance)
 	} else {
 		fmt.Fprintf(w, "<h1>Wrong Parameter<h1>")
@@ -86,27 +73,11 @@ func tokenHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func addBikeHandler(w http.ResponseWriter, r *http.Request) {
-	// Need to run an ETH node locally
-	// Create an IPC based RPC connection to a remote node and instantiate a contract binding
-	// Change the path with your own
-	newBikeId, err := strconv.ParseInt(r.URL.Path[len("/add-bike/"):], 10, 64)
-	if newBikeId == 0 || err != nil {
+	newBikeId, _ := strconv.ParseInt(r.URL.Path[len("/add-bike/"):], 10, 64)
+	if newBikeId == 0 {
 		fmt.Fprintf(w, "<h1>Wrong Parameter<h1>")
 	}
-	conn, err := ethclient.Dial(localEndpoint)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
-	}
-	contract, err := bike.NewBike(common.HexToAddress("0x903b552d9dba24bbb908a064d8568b76923d5501"), conn)
-	if err != nil {
-		log.Fatalf("Failed to instantiate contract: %v", err)
-	}
-	// Need to use the account of the owner to add a bike
-	auth, err := bind.NewTransactor(strings.NewReader(key), "my password")
-	if err != nil {
-		log.Fatalf("Failed to create authorized transactor: %v", err)
-	}
-	tx, err := contract.AddBike(auth, big.NewInt(newBikeId))
+	tx, err := bikeSession.AddBike(big.NewInt(newBikeId))
 	if err != nil {
 		log.Fatalf("Failed to add a bike to the smart contract: %v", err)
 	}
@@ -115,6 +86,39 @@ func addBikeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	conn, err := ethclient.Dial(infuraEndpoint)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum network: %v", err)
+	}
+	bikeContract, err := bike.NewBike(common.HexToAddress(bikeContractAddress), conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate contract: %v", err)
+	}
+	tokenContract, err := token.NewBikeToken(common.HexToAddress(tokenContractAddress), conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate contract: %v", err)
+	}
+
+	auth, err := bind.NewTransactor(strings.NewReader(key), "")
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+
+	bikeSession = &bike.BikeSession{
+		Contract: bikeContract,
+		CallOpts: bind.CallOpts{
+			Pending: true,
+		},
+		TransactOpts: bind.TransactOpts{},
+	}
+	tokenSession = &token.BikeTokenSession{
+		Contract: tokenContract,
+		CallOpts: bind.CallOpts{
+			Pending: true,
+		},
+		TransactOpts: bind.TransactOpts{},
+	}
+
 	http.HandleFunc("/view-bike/", bikeHandler)
 	http.HandleFunc("/view-token/", tokenHandler)
 	http.HandleFunc("/add-bike/", addBikeHandler)
